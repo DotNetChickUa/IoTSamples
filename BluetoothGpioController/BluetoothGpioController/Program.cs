81df52544c3e7c18df22f4452ac9fa121a30f9c9
Анастасія <vladwork97@gmail.com>
2025-10-06 19:27:31 +0300

    Add IPCamera

--- BluetoothGpioController/BluetoothGpioController/Program.cs
+++ BluetoothGpioController/BluetoothGpioController/Program.cs
@@ -10,21 +10,11 @@ using BluetoothGpioController;
  * 6. pair <MAC>
  * 7. trust <MAC>
  * 8. connect <MAC>
-<<<<<<< HEAD
  * 9. scp -r "linux-arm64/*.*" anastasia@raspberrypi-zero-2w.local:/home/anastasia/Projects/BluetoothGpioController
  * 10. ssh anastasia@raspberrypi-zero-2w.local
  * 11. chmod +x /home/anastasia/Projects/BluetoothGpioController/BluetoothGpioController
  * 12. chmod +x /home/anastasia/Projects/BluetoothGpioController/BluetoothGpioController.dll
  * 13. rm BluetoothGpioController -d -r
-=======
- * 9. scp "linux-arm64/*.*" anastasia@raspberrypi-zero-2w.local:/home/anastasia/Projects/BluetoothGpioController
- * 10. ssh anastasia@raspberrypi-zero-2w.local
- * 11. chmod +x /home/anastasia/Projects/BluetoothGpioController/BluetoothGpioController
- * 12. chmod +x /home/anastasia/Projects/BluetoothGpioController/BluetoothGpioController.dll
->>>>>>> fec4208 (Add Reboot and Shutdown commands)
- */
-
-const string logFilePath = "Logs.txt";
 if (File.Exists(logFilePath))
 {
     File.Delete(logFilePath);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 # IoTSamples
+=======
+# IoTSamples
+
+[![Stand With Ukraine](https://img.shields.io/badge/made_in-ukraine-ffd700.svg?labelColor=0057b7)](https://stand-with-ukraine.pp.ua)
+
+License: MIT.
+
+## Projects
+
+- [BluetoothGpioController](BluetoothGpioController)
+- [IPCamera](IPCamera)
+>>>>>>> 971c341 (Add IPCamera)
