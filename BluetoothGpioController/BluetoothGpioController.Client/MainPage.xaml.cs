988af5444fb2ba5247c746428ac887f6392f8d3a
Анастасія <vladwork97@gmail.com>
2025-10-06 23:57:31 +0300

    Control IN OUT

--- BluetoothGpioController/BluetoothGpioController.Client/MainPage.xaml.cs
+++ BluetoothGpioController/BluetoothGpioController.Client/MainPage.xaml.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Diagnostics;
 using System.Text;
 using Plugin.BLE;
@@ -14,6 +15,29 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+=======
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using CommunityToolkit.Maui.Alerts;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace BluetoothController.Client;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<IDevice> Devices { get; set; } = new();
+
+    public ObservableCollection<string> Commands { get; set; } = new()
+    {
+        "GPIO",
+        "REBOOT",
+        "SHUTDOWN"
+    };
+
+<<<<<<< HEAD
+>>>>>>> 6fe7e3e (Control IN OUT)
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
 		var ble = CrossBluetoothLE.Current;
@@ -31,16 +55,32 @@ public partial class MainPage : ContentPage
 				var characteristic = await service.GetCharacteristicAsync(Guid.Parse("12345678-1234-5678-1234-56789abcdef1"));
 <<<<<<< HEAD
 <<<<<<< HEAD
+=======
+<<<<<<<< HEAD:BluetoothGpioController.Client/MainPage.xaml.cs
+<<<<<<< HEAD:BluetoothGpioController.Client/MainPage.xaml.cs
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("19;1"));
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("19;0"));
+=======
+>>>>>>> 6fe7e3e (Control IN OUT)
+<<<<<<< HEAD
 				await characteristic.WriteAsync(Encoding.Default.GetBytes("19;OUTPUT;1"));
 				await characteristic.WriteAsync(Encoding.Default.GetBytes("19;OUTPUT;0"));
 =======
 				await characteristic.WriteAsync(Encoding.Default.GetBytes("10;OUTPUT;1;password"));
 				await characteristic.WriteAsync(Encoding.Default.GetBytes("10;INPUT;0;password"));
 >>>>>>> 9e2922b (Add Password)
+<<<<<<< HEAD
 =======
 				await characteristic.WriteAsync(Encoding.Default.GetBytes("10;Output;1"));
 				await characteristic.WriteAsync(Encoding.Default.GetBytes("10;Input;0"));
 >>>>>>> 8db33c4 (Update readme)
+=======
+>>>>>>> 8b67ad0 (Add Password):BluetoothGpioController/BluetoothGpioController.Client/MainPage.xaml.cs
+========
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("19;OUTPUT;1"));
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("19;OUTPUT;0"));
+>>>>>>>> 6fe7e3e (Control IN OUT):BluetoothGpioController/BluetoothGpioController.Client/MainPage.xaml.cs
+>>>>>>> 6fe7e3e (Control IN OUT)
 			}
 			catch (Exception e)
 			{
@@ -49,4 +89,68 @@ public partial class MainPage : ContentPage
 		};
 		await adapter.StartScanningForDevicesAsync();
 	}
+<<<<<<< HEAD
+=======
+=======
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+
+        Password.Text = Preferences.Get("Password", string.Empty);
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        CommandsPicker.SelectedIndex = 0;
+        await Permissions.RequestAsync<Permissions.Bluetooth>();
+        CrossBluetoothLE.Current.StateChanged += async (s, e) =>
+        {
+            await Toast.Make($"The bluetooth state changed to {e.NewState}").Show();
+        };
+        CrossBluetoothLE.Current.Adapter.DeviceDiscovered += async (s, a) =>
+        {
+            if (!Devices.Contains(a.Device))
+            {
+                Devices.Add(a.Device);
+            }
+        };
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        await CrossBluetoothLE.Current.Adapter.StartScanningForDevicesAsync();
+    }
+
+    private async void SelectableItemsView_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        var device = e.CurrentSelection.OfType<IDevice>().FirstOrDefault();
+        if (device == null)
+        {
+            return;
+        }
+
+        try
+        {
+            await CrossBluetoothLE.Current.Adapter.ConnectToDeviceAsync(device, new ConnectParameters(true, true, ConnectionParameterSet.Balanced));
+            var services = await device.GetServicesAsync();
+            var service = await device.GetServiceAsync(Guid.Parse("12345678-1234-5678-1234-56789abcdef0"));
+            var characteristic =
+                await service.GetCharacteristicAsync(Guid.Parse("12345678-1234-5678-1234-56789abcdef1"));
+            await characteristic.WriteAsync(
+                Encoding.Default.GetBytes($"{Password};{CommandsPicker.SelectedItem};10;OUTPUT;1;"));
+        }
+        catch (Exception ex)
+        {
+            await Toast.Make(ex.Message).Show();
+        }
+    }
+
+    private void Password_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        Preferences.Set("Password", e.NewTextValue);
+    }
+>>>>>>> 65b15a8 (Rework client)
+>>>>>>> 6fe7e3e (Control IN OUT)
 }
--- BluetoothGpioController/BluetoothGpioController/Program.cs
+++ BluetoothGpioController/BluetoothGpioController/Program.cs
@@ -10,6 +10,7 @@ using BluetoothGpioController;
  * 6. pair <MAC>
  * 7. trust <MAC>
  * 8. connect <MAC>
+<<<<<<< HEAD
  * 9. scp -r "linux-arm64/*.*" anastasia@raspberrypi-zero-2w.local:/home/anastasia/Projects/BluetoothGpioController
  * 10. ssh anastasia@raspberrypi-zero-2w.local
  * 11. chmod +x /home/anastasia/Projects/BluetoothGpioController/BluetoothGpioController
@@ -19,13 +20,26 @@ if (File.Exists(logFilePath))
 {
     File.Delete(logFilePath);
 }
+=======
+<<<<<<<< HEAD:BluetoothGpioController/Program.cs
+ * 7. scp "linux-arm64/*.*" anastasia@raspberrypi-zero-2w.local:/home/anastasia/Projects/BluetoothGpioController
+========
+ * 9. scp "linux-arm64/*.*" anastasia@raspberrypi-zero-2w.local:/home/anastasia/Projects/BluetoothGpioController
+ * 10. ssh anastasia@raspberrypi-zero-2w.local
+>>>>>>>> 6fe7e3e (Control IN OUT):BluetoothGpioController/BluetoothGpioController/Program.cs
+ */
+>>>>>>> 6fe7e3e (Control IN OUT)
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 builder.Services.Configure<AppSettings>(builder.Configuration);
+<<<<<<< HEAD
 #if DEBUG
 builder.Services.AddLogging(b => b.AddConsole().AddProvider(new FileSystemLoggerProvider(logFilePath)));
 #endif
+=======
+builder.Services.AddLogging(b => b.AddConsole());
+>>>>>>> 6fe7e3e (Control IN OUT)
 
 var host = builder.Build();
 host.Run();
