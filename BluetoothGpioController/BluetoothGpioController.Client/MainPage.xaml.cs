9e80531c6321ab11496d21f5d1a85515859b2262
Анастасія <vladwork97@gmail.com>
2025-10-06 23:01:31 +0300

    Update readme

---
+++ BluetoothGpioController/BluetoothGpioController.Client/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Exceptions;
+
+namespace LampController.Client;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		var ble = CrossBluetoothLE.Current;
+		var adapter = CrossBluetoothLE.Current.Adapter;
+		ble.StateChanged += (s, e) =>
+		{
+			Debug.WriteLine($"The bluetooth state changed to {e.NewState}");
+		};
+		adapter.DeviceDiscovered += async (s, a) =>
+		{
+			try
+			{
+				await adapter.ConnectToDeviceAsync(a.Device);
+				var service = await a.Device.GetServiceAsync(Guid.Parse("12345678-1234-5678-1234-56789abcdef0"));
+				var characteristic = await service.GetCharacteristicAsync(Guid.Parse("12345678-1234-5678-1234-56789abcdef1"));
+<<<<<<< HEAD
+<<<<<<< HEAD
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("19;OUTPUT;1"));
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("19;OUTPUT;0"));
+=======
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("10;OUTPUT;1;password"));
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("10;INPUT;0;password"));
+>>>>>>> 9e2922b (Add Password)
+=======
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("10;Output;1"));
+				await characteristic.WriteAsync(Encoding.Default.GetBytes("10;Input;0"));
+>>>>>>> 8db33c4 (Update readme)
+			}
+			catch (Exception e)
+			{
+				// ... could not connect to device
+			}
+		};
+		await adapter.StartScanningForDevicesAsync();
+	}
+}
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # IoTSamples
 =======
+=======
+>>>>>>> ac3e069 (Update readme)
 # IoTSamples
 
 [![Stand With Ukraine](https://img.shields.io/badge/made_in-ukraine-ffd700.svg?labelColor=0057b7)](https://stand-with-ukraine.pp.ua)
@@ -9,6 +12,10 @@ License: MIT.
 
 ## Projects
 
+<<<<<<< HEAD
 - [BluetoothGpioController](BluetoothGpioController)
 - [IPCamera](IPCamera)
 >>>>>>> 971c341 (Add IPCamera)
+=======
+- [BluetoothGpioController](BluetoothGpioController)
+>>>>>>> ac3e069 (Update readme)
